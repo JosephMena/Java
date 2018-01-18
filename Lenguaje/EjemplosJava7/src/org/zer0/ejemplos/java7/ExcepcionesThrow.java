3b307f7c43097f63402a7c2fac1f87e8365e9bb9
pjmena <pjmena@PANALISTA37.onp.net>
2018-01-18 10:54:54 -0500

    Lo nuevo en excepciones

---
+++ Lenguaje/EjemplosJava7/src/org/zer0/ejemplos/java7/ExcepcionesThrow.java
@@ -0,0 +1,29 @@
+package org.zer0.ejemplos.java7;
+
+public class ExcepcionesThrow {
+
+	// En la version 6 de java este codigo hubiera arrojado la un error de compilacion, indicandonos que no se esta controlando la excepcion
+	// Exception
+	//Apartir de Java 7, el JDK identifica las excepciones que se van a lanzar y las compara contra la cabecera del metodo throws. 
+	
+	
+	
+	public void m1(int v)  throws Ex1 , Ex2 {
+        try {
+            if (v < 1) {
+                throw new Ex1();
+            } 
+            else
+               if ( v > 100 ) {
+                   throw new Ex2();
+               }
+        } 
+        catch (Exception ex) {
+            throw ex;   // excepción no reportada java.lang.Exception
+        }
+    }
+	
+}
+
+class Ex1 extends Exception {private static final long serialVersionUID = 7200285423877250795L; }
+class Ex2 extends Exception {private static final long serialVersionUID = -3116320937052406528L; }
