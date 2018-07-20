b64a819b6d4931c0a6c7c4d5cac6c5534103d93a
Joseph Mena <ip.zer0@gmail.com>
2018-07-19 17:07:03 -0700

    Streams primitivos

--- Lenguaje/EjemplosJava8/src/org/zer0/ejemplos/java8/streams/Test.java
+++ Lenguaje/EjemplosJava8/src/org/zer0/ejemplos/java8/streams/Test.java
@@ -1,8 +1,12 @@
 package org.zer0.ejemplos.java8.streams;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.OptionalDouble;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import org.zer0.ejemplo.java8.util.Persona;
@@ -12,8 +16,12 @@ public class Test {
 	public static void main(String[] args) {
 		Test test=new Test();
 		//test.operacionFilter();
-		test.operacionMap();
-		test.operacionMap2();
+		//test.operacionMap();
+		//test.operacionMap2();
+		//test.operacionFindFirst();
+		//test.operacionAnyMatch();
+		//test.intStream();
+		test.operacionSorted();
 	}
 
 	//Filter permite que dado una lista, se filtre solo algunos de los elementos de la lista
@@ -50,9 +58,33 @@ public class Test {
 	
 	private void operacionFindFirst() {
 		Stream<String> flujo=Stream.of("joseph","cesar","mena","sihuacollo");
-		flujo.findFirst();
+		Optional<String> elemento=flujo.findFirst();
+		System.out.println(elemento.get());
+		
 	}
 	
+	private void operacionAnyMatch() {
+		Stream<String> flujo=Stream.of("Joseph","Mena","Sihuacollo","peruano");
+		boolean contienePeru=flujo.anyMatch(param->{return param.contains("peru");});
+		System.out.println(contienePeru);
+	}
+	
+	private void intStream() {
+		IntStream stream=Arrays.stream(new int[] {1,2,4,6,2});
+		OptionalDouble op=stream.average();
+		System.out.println(op.getAsDouble());
+	}
+	
+	private void operacionSorted() {
+		Stream<String> array=Arrays.stream(new String[]{"Paul","Erik","Joseph","Cesar","Teodomiro"});
+		array.sorted().forEach(System.out::println);
+	}
+	
+	private void operacionSorted2() {
+		Stream<String> array=Arrays.stream(new String[]{"Paul","Erik","Joseph","Cesar","Teodomiro"});
+		array.sorted().forEach(System.out::println);
+		
+	}
 }
 
 
